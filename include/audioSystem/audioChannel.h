c4c8259f3802cf9f052dc0078ee547d01ede1320
Daho0980 <0980daho@gmail.com>
2025-11-15 18:17:49 +0900

    Refactor: audioChannel 헤더 적용

--- include/audioSystem/audioChannel.h
+++ include/audioSystem/audioChannel.h
@@ -6,6 +6,28 @@
 #include "audioStruct.h"
 
 
+#define VOLUME_SCALE 0.0244200244f
+
+struct LocalVol {
+    float baseVolume;
+    float currVolume;
+
+    bool  fadeActive ;
+    float decayFactor;
+    float target     ;
+};
+inline struct LocalVol LocalVol_init() {
+    struct LocalVol out = {
+        .baseVolume  = 100.0,
+        .currVolume  = 1.0,
+        .fadeActive  = false,
+        .decayFactor = 0.0,
+        .target      = 0.0
+    };
+
+    return out;
+}
+
 typedef struct AudioChannel_t AudioChannel_t;
 
 void destroyAllChannelThreads(AudioManager* mgr, int index, bool force);
