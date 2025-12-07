bde96be08160412b2416cce360f51d63f9f44b18
oɥɐꓷ <0980daho@gmail.com>
2025-12-07 11:41:31 +0900

    Docs: 주석 수정

--- src/audioSystem/mac/performance/arm/easing/smootherstep.s
+++ src/audioSystem/mac/performance/arm/easing/smootherstep.s
@@ -24,8 +24,8 @@ _smootherstep:
     ldr         s6, CONST_15_0
     ldr         s7, CONST_10_0
 
-    fmul        s3, s0, s0          // s0**2 -> s3
-    fmul        s3, s3, s0          // s3*s0 -> s3
+    fmul        s3, s0, s0          // s0 ** 2 -> s3
+    fmul        s3, s3, s0          // s3 * s0 -> s3
     // -> s3 = t**3
 
     fmul        s4, s0, s5          // s0 * 6 -> s4
@@ -38,7 +38,7 @@ _smootherstep:
     // -> s3 = f(t) scale 0~1
 
     fsub        s4, s2, s1          // end - start -> s4
-    fmul        s3, s3, s4          // (end-start)*f(t) -> s3
+    fmul        s3, s3, s4          // (end-start) * f(t) -> s3
     fadd        s0, s3, s1          // ...+start -> s0
 
     ret
